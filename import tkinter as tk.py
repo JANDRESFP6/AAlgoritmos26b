5ceeed82556343055a1bd40a42bbd926cd665bf3
JANDRESFP6 <jose.flores3545@alumnos.udg.mx>
2026-08-27 14:54:30 -0600

    Primer commit

---
+++ import tkinter as tk.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+#Aqui va a ir la creacion de la funcion 
+def saludar():
+    nombre=entrada.get().strip()
+    if not nombre:
+        nombre="mundo"
+    lbl.config(text=f"Hola, {nombre}")
+
+root = tk.Tk()
+root.title("Saludador de compas")
+root.geometry("360x220")
+lbl= tk.Label(root, text="Hola, escribe tu nombre y presiona el boton")
+lbl.pack(pady=30)#pady es el espacio de la posicion en Y
+entrada=tk.Entry(root)
+entrada.pack(pady=10)
+
+#Creacion de boton 
+btn=tk.Button(root,text="Saludar",command=saludar) 
+btn.pack(pady=10)
+
+root.mainloop()
